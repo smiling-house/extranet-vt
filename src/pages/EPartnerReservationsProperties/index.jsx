f1e79b036e81975a4bd350bc30dd7a6cd84f3e30
smilingcloud21 <smilingcloud21@gmail.com>
2025-10-27 12:16:31 +0530

    constants.X_API_KEY

--- src/pages/EPartnerReservationsProperties/index.jsx
+++ src/pages/EPartnerReservationsProperties/index.jsx
@@ -306,8 +306,8 @@ useEffect(() => {
     const token2 = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X29iamVjdF9pZCI6Mzk5MTU4NzUsInVzZXJfaWQiOiI0MDY2NTAyMSIsInVzZXJfbmFtZSI6InN5c3RlbStsdW5hLTh5NXljIiwic2NvcGUiOlsiYnJpdm8uYXBpIl0sImF0aSI6ImI5MTliYmJiLTA1ZWItNDlmOC05MjlhLWM0MTJlYzY3NWI2YyIsImlzc3VlZCI6IjE2NzUzNzA2NDMzNzMiLCJleHAiOjIyOTczMzM3MjcsInNlcnZpY2VfdG9rZW4iOm51bGwsImF1dGhvcml0aWVzIjpbIlJPTEVfU1VQRVJfQURNSU4iLCJST0xFX0FETUlOIl0sImp0aSI6IjExODQzYjg2LWIyYzUtNGMwNS1hYWZlLTcxZTI4NGIyNjNlOCIsImNsaWVudF9pZCI6IjkzOTFlYjVkLWUwNmUtNDY4MS1iNTdhLWQwZTU3NDhhM2RlZSIsIndoaXRlX2xpc3RlZCI6ZmFsc2V9.Mqmx7onIVz_EVAunhwqBAhAmlsGXMQ18hh_EV_61KQIpaGXlrgXgx1hOOdNWLFriG3Un6jfS7H7vwMAYmBT6-8yl9L7VB7Cpxva49XozuSJazQ42UDDlTOsnWAmatzmFna-Uzjc8MDfVQbR8AwMiFq_Jb9ViaJ4XBkj2KhEKs1g'
 
     const userRequest = axios.create({
-        headers: {
-            'x-api-key': 'swwSK4tqWyYHP4GvRF7qHNOoSGVs7nwSekPKyLQD'
+        headers: { 
+            'x-api-key': constants.X_API_KEY,
             // Authorization: `Bearer ${token2}`
             // Authorization: `Bearer ${Epartner.bearerToken}`
         }
@@ -321,14 +321,15 @@ const limit = constants.PAGING_LISTING_SIZE; // Number of items to take
 const skip = clientPagingFrom-1;  // Number of items to skip
         // filter=pending&sortBy=startDate:desc
         const params= {
-            // limit: constants.PAGING_LISTING_SIZE, 
-            // skip: 0,
+            limit: constants.PAGING_LISTING_SIZE, 
+            skip: 0,
             // filter: 'pending',
             sortBy: 'startDate:desc'
         }
     
     console.log('getting from /listings:',params)
     const queryString = Object.keys(params).map(key => key + '=' + params[key]).join("&")
+
 
     if (true) {
         const shubSearch=constants.SHUB_URL+'/eps/get-all-reservations'+'?';
